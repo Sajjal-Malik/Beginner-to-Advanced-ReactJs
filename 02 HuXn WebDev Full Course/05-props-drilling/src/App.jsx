52b3655bbbce728a0c796ec53e833452228d5b48
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-02-19 12:54:59 +0500

    Props drilling Project Code pushed

---
+++ 02 HuXn WebDev Full Course/05-props-drilling/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ComponentA from './components/ComponentA'
+
+function App() {
+
+  const name = "Malik";
+
+  return (
+    <>
+      <ComponentA name={name} />
+    </>
+  )
+}
+
+export default App
