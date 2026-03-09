0b83e90bab299993b2aa8b33491e76d294523312
Sajjal Malik <sajjalmalik.sm@gmail.com>
2026-03-09 13:57:56 +0500

    Use Reducer hook code pushed

--- 02 HuXn WebDev Full Course/08-useReducer-hook/src/App.jsx
+++ 02 HuXn WebDev Full Course/08-useReducer-hook/src/App.jsx
@@ -1,14 +1,32 @@
 import { useReducer } from 'react'
 import './App.css'
 
+const initialState = { count: 0 };  // Initial State as Object
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "reset":
+      return { ...state, count: 0 };
+    default:
+      return state;
+  }
+}
+
 function App() {
 
-  useReducer
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <>
-
-    </>
+    <div>
+      <h1>Count: {state.count}</h1>
+      <button onClick={() => { dispatch({ type: "increment" }) }}>+</button>
+      <button onClick={() => { dispatch({ type: "decrement" }) }}>-</button>
+      <button onClick={() => { dispatch({ type: "reset" }) }}>Reset</button>
+    </div>
   )
 }
 
